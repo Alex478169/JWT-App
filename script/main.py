9b4cbdb30c4d122a5233dc7fa658384a15925d08
Alex478169 <alessandro.mor@outlook.it>
2025-04-10 12:28:33 +0000

    Writing of the first version of JWT Configuration JSON file

---
+++ script/main.py
@@ -0,0 +1,11 @@
+
+def main:
+    '''
+    ehmmm non lo so, immagino che qui debba creare una web app, quindi inserire delle richieste HTTP. Ora che ci penso
+    questo sarebbe possibile con la classe "request"? oppure è usata principalmente per fare richieste http ad altri siti?
+    '''
+    return ""
+
+
+if __name__ == "main":
+    main()
