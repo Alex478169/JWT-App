c2037d5aa653e83a829d96154688fbd2f26bc289
Alex478169 <alessandro.mor@outlook.it>
2025-05-03 15:17:17 +0000

    development of fromCSVtoSQL and writing of dbOperations functions comments

--- script/dbOperations.py
+++ script/dbOperations.py
@@ -2,6 +2,16 @@ import psycopg2
 import pandas as pd
 
 def viewDataTable(config, table: str):
+    """
+    (view the table content)visualizza il contenuto di una tabella
+
+    args:
+        config: file di configurazione che permette la connessione con il database postgresql
+        table (str): nome della tabella
+
+    returns:
+        str: errore di connessione al database
+    """
     try:
         # connecting to the PostgreSQL server
         with psycopg2.connect(**config) as conn:
@@ -13,25 +23,49 @@ def viewDataTable(config, table: str):
         return str(error)
 
 def insertJobApplication(config, appDate: str, contEmail: str, compName: str, jobTitle: str, jobUrl: str, resName: str):
+    """
+    inserimento di una riga nella tabella job_applications
+
+    args:
+        config: file di configurazione che permette la connessione con il database postgresql
+        appDate: colonna di application_date
+        contEmail: colonna di contact_email
+        compName: colonna di company_name
+        jobTitle: colonna di job_title
+        jobUrl: colonna di job_url
+        resName: colonna di resume_name
+
+    returns:
+        str: errore di connessione al database postgresql
+    """
      try:
-        # connecting to the PostgreSQL server
         with psycopg2.connect(**config) as conn:
             cur = psycopg2.cursor()
             cur.execute("INSERT INTO job_application(Application_Date, Contact_Email, Company_Name, Job_Title, Job_Url, Resume_Name) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}');", appDate, contEmail, compName, jobTitle, jobUrl, resName)
+            cur.commit()
             return conn
      except (psycopg2.DatabaseError, Exception) as error:
-        return(error)
+        return str(error)
 
-def connect(config):
+'''def connect(config):
     try:
         with psycopg2.connect(**config) as conn:
             return "Good job boy! you are connected to the database!"
     except (psycopg2.DatabaseError, Exception) as error:
-            return str(error)
+            return str(error)'''
 
 def showValues(config, column: str):
+    """
+    mostra i valori di una colonna specifica
+
+    args:
+        config: file di configurazione che permette la connessione con il database postgresql
+        column: nome di una colonna
+    
+    returns:
+        str: errore di connessione al database
+    """
     try:
-        # connecting to the PostgreSQL server
         with psycopg2.connect(**config) as conn:
             cur = conn.cursor()
             cur.execute(f"select {column} from job_application;")
@@ -41,6 +75,17 @@ def showValues(config, column: str):
         return str(error)
 
 def insertValues(config, username:str, password:str):
+    """
+    esegue la registrazione di un utente
+
+    args:
+        config: file di configurazione che permette la connessione con il database postgresql
+        username: nome utente dell'utente registrato
+        password: password dell'utente registrato
+
+    returns:
+        str: errore di connessione al database postgresql
+    """
     try:
         # connecting to the PostgreSQL server
         with psycopg2.connect(**config) as conn:
@@ -57,19 +102,23 @@ def insertValues(config, username:str, password:str):
         return str(error)
 
 def compareValues(config, username:str, password:str):
+    """
+    compara la password di un utente specifico con quello inserito
+    
+    args:
+        config: file di configurazione che permette la connessione con il database postgresql
+        username: username di un utente usato per cercare una password specifica
+        password: password inserita dall'utente nella schermata di login che viene comparata con la password dell'utente specifico
+    
+    returns:
+        str: l'errore relativo alla connessione al database postgresql
+    """
     try:
-        # connecting to the PostgreSQL server
         with psycopg2.connect(**config) as conn:
-            #creazione di un cursore
             cur = conn.cursor()
-
-            #esecuzione di una query che trova la password dell'utente richiesto
             cur.execute(f"SELECT password FROM users WHERE username='{username}'")
-            #e prende il valore
             value = cur.fetchone()
-            #per qualche strano motivo non mi stampa il valore della query
-            print(value)
-               
+                           
             if password in value[0]:
                 return True
             else:
@@ -78,14 +127,38 @@ def compareValues(config, username:str, password:str):
     except (psycopg2.DatabaseError, Exception) as error:
         return str(error)
 
-def fromCSVToSQL(config, file: str, table:str):
-    df = pd.read_csv(file)
-    #modifica il contenuto di una cella su una riga esatta (tipo query di UPDATE)
+def fromCSVToSQL(config, file: str):
+    """
+    imagazzina i dati presenti in un file CSV (.csv) all'interno di un database postgresql 
+
+    args:
+        config: file di configurazione che permette la connessione con il database postgresql
+        file(str): nome del file da cui prendere i dati da inserire nel database 
+
+    returns:
+        str: errore di connessione al database postgresql
+    """
+    insert_queries=[]
+    
+    while True:
+        try:
+            df = pd.read_csv(file)
+            break
+        except Exception(FileNotFoundError) as error:
+            print(error)
+            break
+        except Exception(ValueError) as error:
+            print(error)
+            break
+        except Exception(ImportError) as error:
+            print(error)
+            break
+        except Exception(PermissionError) as error:
+            print(error)
+            break
+
     df.loc[df["Application Date"] == "9/9/24, 8:21 AM", "Company Name"] = "Tecne - Gruppo Autostrade per Italia"
     df.to_csv(file)
-    insertQueries=[]
-
-        #itera sulle righe
     for _,row in df.iterrows():
         application_date = row["Application Date"].replace("'", "''")
         contact_email = row["Contact Email"].replace("'", "''")
@@ -93,32 +166,90 @@ def fromCSVToSQL(config, file: str, table:str):
         job_title = row["Job Title"].replace("'", "''")
         job_url = row["Job Url"].replace("'", "''")
         resume_name = row["Resume Name"].replace("'", "''")
-        print(application_date)
-
-        sqlInsert = f"INSERT INTO {table}(application_date, contact_email, company_name, job_title, job_url, resume_name) VALUES ('{application_date}', '{contact_email}', '{company_name}', '{job_title}', '{job_url}', '{resume_name}');"
-        insertQueries.append(sqlInsert)
         try:
             with psycopg2.connect(**config) as conn:
                 cur = conn.cursor()
                 for sql in insertQueries:    
-                    cur.execute(sql)
-                    conn.commit()
+                    insertJobApplication(config, application_date, contact_email, company_name, job_title, job_url, resume_name)
         except (psycopg2.DatabaseError, Exception) as error:
             return str(error)
+    
     return "tutto è andato buon fine"
 
-def searchSomething(config, company:str):
-    try:
-        # connecting to the PostgreSQL server
-        with psycopg2.connect(**config) as conn:
-            #creazione di un cursore
-            cur = conn.cursor()
+def fromExcelToSQL(config, file:str):
+    """
+    imagazzina i dati presenti in un file Excel (.xslx) all'interno di un database postgresql 
 
-            #esecuzione di una query che trova la password dell'utente richiesto
+    args:
+        config: file di configurazione che permette la connessione con il database postgresql
+        file: nome del file da cui prendere i dati da inserire nel database
+
+    ritorna:
+        str: errore di connessione al database postgresql
+    """    
+    val = []
+    val_new = []
+
+    while True:
+        try:
+            df = pd.read_excel(file)
+            break
+        except Exception(FileNotFoundError) as error:
+            print(error)
+            break
+        except Exception(ValueError) as error:
+            print(error)
+            break
+        except Exception(ImportError) as error:
+            print(error)
+            break
+        except Exception(PermissionError) as error:
+            print(error)
+            break
+           
+    for col_name in df.columns: 
+        for col in df.itertuples():
+            row_value = getattr(col, col_name, "N/A")
+            val.append(row_value)    
+    for i in val:
+        num = 0
+        value = val[num].replace("’", "")
+        val_new.append(value)
+        num += 1
+    
+    try:
+        with psycopg2.connect(**config) as conn:
+            columns = []
+            for cols in df.columns:
+                Cols = "'" + cols + "' ,"
+                columns.append(cols)
+                for i in df.itertuples():
+                    for n in columns:
+                        num = 0
+                        row_val = getattr(col, columns[num], "N/A")
+                        num += 1            
+                        cur = conn.cursor()
+                        cur.execute(f"insert into job_application({columns}) VALUES ({row_val});")
+    except (psycopg2.DatabaseError, Exception) as error:
+        return str(error)
+         
+def searchSomething(config, company:str):
+    """
+    Individua e visualizza un risultato nella tabella dell'endpoint "\candidature"
+    
+    args:
+        config:file di configurazione che permette la connessione con il database postgresql
+        company:nome dell'azienda utilizzato per fare la ricerca
+
+    returns:
+        str: errore di connessione al database
+    """
+
+    try:
+        with psycopg2.connect(**config) as conn:
+            cur = conn.cursor()
             cur.execute(f"select distinct * from job_application where company_name='{company}';")
-            #e prende il valore
             value = cur.fetchone()
-            print(value)
             return value
     except (psycopg2.DatabaseError, Exception) as error:
         return str(error)
--- script/main.py
+++ script/main.py
@@ -1,13 +1,52 @@
-from flask import Flask, render_template, request, url_for
-from dbOperations import connect, showValues, insertValues, compareValues, fromCSVToSQL, viewDataTable, searchSomething
+from dbOperations import *
 from config import load_config
-import psycopg2
-import pandas as pd
+from flask_jwt_extended import JWTManager, create_access_token
+from authlib.integrations.flask_client import OAuth
+import os
+#Prova ad importare le librerie corrette
+try:
+    from flask import Flask, render_template, request, url_for
+except:
+    print("non è stato importato il pacchetto 'flask' per utilizzare il microframework flask")
+    print("Per installarlo, usare il comando 'pip install -r requirements.txt' nel terminale")
 
-#potrei creare un sito web in cui viene usato il jwt token per accedere al sito e fare poi cose
+try: 
+    import psycopg2
+except:
+    print("non è stato importato il pacchetto 'psycopg2' per la comunicazione con il database postgresql.")
+    print("Per installarlo, usare il comando 'pip install -r requirements.txt' nel terminale")
+
+try:
+    import pandas as pd
+except:
+    print("non è stato installato il pacchetto 'pandas'.")
+    print("Per installarlo, usare il comando 'pip install -r requirements.txt' nel terminale")
 
 app = Flask(__name__)
 
+#imposta la chiave segreta per le sessioni Flask utilizzando una variabile d'ambiente
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "NadaAmePiruAttoCarrell1253484")
+#configurazione delle chiavi di sicurezza utilizzando variabili d'ambiente
+app.config["secret"] = os.environ.get("secret", "v7+MPnCh66xstznZq9mJ7nMMCrXxmdZrVIxrHuUdr18K1v0gi2yV2sSudD2ohb8yfGM8AuxbyKjiz9BwcSOoRg==")
+
+#inizializza JWT
+jwt = JWTManager(app)
+
+#autenticazione con linkedin
+linkedin = oauth.register(
+    "linkedin",
+    client_id = app.config["LINKEDIN_CLIENT_ID"]
+    client_secret=app.config["LINKEDIN_CLIENT_SECRET"]
+    authorize_url="https://www.linkedin.com/oauth/v2/authorization"
+    authorize_params=None,
+    access_token_url="https://www.linkedin.com/oauth/v2/accessToken",
+    access_token_params=None,
+    refresh_token_url=None,
+    api_base_url='https://api.linkedin.com/v2/',
+    client_kwargs={'scope': 'r_liteprofile r_emailaddress w_member_social'}
+)
+
+#pagina di registrazione
 @app.route("/", methods=['POST', 'GET'])
 def register():
     if request.method == "POST":
@@ -22,6 +61,8 @@ def register():
     
     return render_template("register.html")
 
+
+#pagina di login
 @app.route("/login", methods=['POST', 'GET'])    
 def login():
     with psycopg2.connect(**config) as conn:
@@ -35,20 +76,29 @@ def login():
                 return render_template("login.html", error="credenziali errate")
     return render_template("login.html")
 
-
+#pagina di home
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-@app.route("/options")
+#opzioni per la tabella candidature
+@app.route("/options", methods=['GET', 'POST'])
 def candidatureOptions():
-    return render_template("options.html")
+    #io qui, a partire dal momento in cui ho inserito un file, ed il formato è corretto, io dovrei passare il file (utilizzare la funzione fromExcelToSQL()) e caricarlo nel database
+    #la conversione del csv è già fatto, ma ora devo prendere il file csv e caricarlo. dovrei prendere solamente il percorso file e bon, risolta la situazione
+    if request.method == "POST":
+        File="/home/ale/.cache/.fr-L3ZH42/Jobs/uarewelcome.xlsx"
+        fromExcelToSQL(config, File)
+        archivio = request.files.get("archive")
+        '''if archivio.filename != "":
+            print(archivio.filename)'''
 
+#pagina tabella candidature
 @app.route("/candidature", methods=["GET"])
 def candidature():
     file = "/home/ale/Downloads/Jobs/Job_Applications.csv"
     table = "job_application"
-
+    
     company=request.form.get("company")
     email=request.form.get("email")
     
@@ -62,12 +112,11 @@ def candidature():
     urlJobs = showValues(config, "job_url")
     resumeNames = showValues(config, "resume_name")
     
-    print(applicationDates)
     try:
         if viewDataTable(config, "job_application") is not None:
             pass
         else:
-            fromCSVToSQL()
+            fromCSVToSQL(file, table)
             return render_template("tabellaCandidature.html", applicationDates=applicationDates, contactsEmail=contactsEmail, companyNames=companyNames, jobtitles=jobtitles, urlJobs=urlJobs, resumeNames=resumeNames)
     except (psycopg2.DatabaseError, Exception) as error:
         return str(error)
@@ -79,3 +128,40 @@ if __name__ == "__main__":
     connect(config)
     app.run(debug=True)
 
+
+
+
+"""
+def login_linkedin():
+    redirect_uri = url_for('authorize_google', _external=True)
+    nonce = secrets.token_urlsafe(16)  # Genera un nonce casuale
+    session['oauth_nonce'] = nonce  # Salva il nonce nella sessione
+    return google.authorize_redirect(redirect_uri, nonce=nonce)
+
+# Callback per Google OAuth
+@app.route('/authorize/google')
+def authorize_google():
+    try:
+        token = google.authorize_access_token()
+        nonce = session.pop('oauth_nonce', None)
+        if not nonce:
+            return jsonify({'msg': 'Nonce non trovato in sessione, riprovare'}), 400
+        
+        user_info = google.parse_id_token(token, nonce=nonce)
+        
+        # Verifica se l'utente esiste già nel database
+        user = users.find_one({'email': user_info['email']})
+        
+        if not user:
+            # Se l'utente non esiste, registralo
+            users.insert_one({'email': user_info['email'], 'password': None})
+
+        # Genera un token JWT
+        access_token = create_access_token(identity={'email': user_info['email']})
+        return jsonify({'token': access_token}), 200
+
+    except Exception as e:
+        print(f"Errore durante l'autenticazione con Google: {e}")
+        return jsonify({'msg': 'Errore durante l\'autenticazione con Google', 'error': str(e)}), 500
+
+"""
